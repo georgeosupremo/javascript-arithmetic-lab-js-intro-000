b880588d93193ec69cb128691215ff0178c7a4d9
george.burton <george.burton@wework.com>
2019-07-26 21:44:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b){
+  var outcome= a + b;
+  return outcome
+}
+function subtract(a,b){
+  var outcome= a - b;
+  return outcome
+}
+function multiply(a,b){
+  var outcome= a*b;
+  return outcome
+}
+
+function divide(a,b){
+  var outcome= a/b;
+  return outcome
+}
+
+function inc(n){
+  n++
+var outcome= n++;
+return outcome
+}
+
+function dec(n){
+  n--
+  var outcome=  n--
+  return outcome
+}
+
+function makeInt(n){
+  parseInt('n')
+  return n
+}
